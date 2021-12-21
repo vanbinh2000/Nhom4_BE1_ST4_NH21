5cb3bd56d0a084c4466f65fbc2b72f67e1d6dab8
baonguyen212002 <baonguyen212002@gmail.com>
2021-12-21 18:39:30 +0700

    thêm topselling

--- index.php
+++ index.php
@@ -315,33 +315,416 @@ include "header.php";?>
 					<!-- section title -->
 					<div class="col-md-12">
 						<div class="section-title">
-							<h3 class="title">Top Selling</h3>
+							<h3 class="title">Top selling</h3>
 							<div class="section-nav">
 								<ul class="section-tab-nav tab-nav">
-								<li class="active"><a data-toggle="tab" href="#taba">Laptops</a></li>
-									<li><a data-toggle="tab" href="#tabb">Smartphones</a></li>
-									<li><a data-toggle="tab" href="#tabc">PC</a></li>
-									<li><a data-toggle="tab" href="#tabd">Accessories</a></li>
-									<li><a data-toggle="tab" href="#tabe">Tablet</a></li>
+									<li class="active"><a data-toggle="tab" href="#tab6">Smartphones</a></li>
+									<li><a data-toggle="tab" href="#tab7">Laptops</a></li>
+									<li><a data-toggle="tab" href="#tab8">Accessories</a></li>
+									<li><a data-toggle="tab" href="#tab9">PC</a></li>
+									<li><a data-toggle="tab" href="#tab10">Tablet</a></li>
 								</ul>
 							</div>
 						</div>
 					</div>
 					<!-- /section title -->
 
+					<!-- Products tab & slick -->
+					<div class="col-md-12">
+						<div class="row">
+							<div class="products-tabs">
+								<!-- tab -->
+								<div id="tab6" class="tab-pane fade in active">
+									<div class="products-slick" data-nav="#slick-nav-6">
+										<?php foreach ($gettopsellingSmartphone as $value): ?>
+										<!-- product -->
+										<div class="product">
+											<div class="product-img">
+												<img src="./img/<?php echo $value['image'] ?>" alt="">
+												<div class="product-label">
+													
+												</div>
+											</div>
+											<div class="product-body">
+												<p class="product-category">Category</p>
+												<h3 class="product-name"><a href="#"><?php echo $value['name'] ?></a></h3>
+												<h4 class="product-price"><?php echo number_format($value['price']) ?> VND</h4>
+												<div class="product-rating">
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+												</div>
+												<div class="product-btns">
+													<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
+													<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+												</div>
+											</div>
+											<div class="add-to-cart">
+												<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+											</div>
+										</div>
+										<!-- /product -->
+										<?php endforeach; ?>
+									</div>
+									<div id="slick-nav-6" class="products-slick-nav"></div>
+								</div>
+								<!-- /tab -->
+								<!-- tab -->
+								<div id="tab7" class="tab-pane">
+									<div class="products-slick" data-nav="#slick-nav-7">
+										<?php foreach ($gettopsellingLaptops as $value): ?>
+										<!-- product -->
+										<div class="product">
+											<div class="product-img">
+												<img src="./img/<?php echo $value['image'] ?>" alt="">
+												<div class="product-label">
+													
+												</div>
+											</div>
+											<div class="product-body">
+												<p class="product-category">Category</p>
+												<h3 class="product-name"><a href="#"><?php echo $value['name'] ?></a></h3>
+												<h4 class="product-price"><?php echo number_format($value['price']) ?> VND</h4>
+												<div class="product-rating">
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+												</div>
+												<div class="product-btns">
+													<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
+													<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+												</div>
+											</div>
+											<div class="add-to-cart">
+												<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+											</div>
+										</div>
+										<!-- /product -->
+										<?php endforeach; ?>
+									</div>
+									<div id="slick-nav-7" class="products-slick-nav"></div>
+								</div>
+								<!-- /tab -->
+								<!-- tab -->
+								<div id="tab8" class="tab-pane">
+									<div class="products-slick" data-nav="#slick-nav-8">
+										<?php foreach ($gettopsellingAccessories as $value): ?>
+										<!-- product -->
+										<div class="product">
+											<div class="product-img">
+												<img src="./img/<?php echo $value['image'] ?>" alt="">
+												<div class="product-label">
+													
+												</div>
+											</div>
+											<div class="product-body">
+												<p class="product-category">Category</p>
+												<h3 class="product-name"><a href="#"><?php echo $value['name'] ?></a></h3>
+												<h4 class="product-price"><?php echo number_format($value['price']) ?> VND</h4>
+												<div class="product-rating">
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+												</div>
+												<div class="product-btns">
+													<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
+													<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+												</div>
+											</div>
+											<div class="add-to-cart">
+												<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+											</div>
+										</div>
+										<!-- /product -->
+										<?php endforeach; ?>
+									</div>
+									<div id="slick-nav-8" class="products-slick-nav"></div>
+								</div>
+								<!-- /tab -->
+								<!-- tab -->
+								<div id="tab9" class="tab-pane">
+									<div class="products-slick" data-nav="#slick-nav-9">
+										<?php foreach ($gettopsellingPC as $value): ?>
+										<!-- product -->
+										<div class="product">
+											<div class="product-img">
+												<img src="./img/<?php echo $value['image'] ?>" alt="">
+												<div class="product-label">
+													
+												</div>
+											</div>
+											<div class="product-body">
+												<p class="product-category">Category</p>
+												<h3 class="product-name"><a href="#"><?php echo $value['name'] ?></a></h3>
+												<h4 class="product-price"><?php echo number_format($value['price']) ?> VND</h4>
+												<div class="product-rating">
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+												</div>
+												<div class="product-btns">
+													<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
+													<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+												</div>
+											</div>
+											<div class="add-to-cart">
+												<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+											</div>
+										</div>
+										<!-- /product -->
+										<?php endforeach; ?>
+									</div>
+									<div id="slick-nav-9" class="products-slick-nav"></div>
+								</div>
+								<!-- /tab -->
+								<!-- tab -->
+								<div id="tab10" class="tab-pane">
+									<div class="products-slick" data-nav="#slick-nav-10">
+										<?php foreach ($gettopsellingTablets as $value): ?>
+										<!-- product -->
+										<div class="product">
+											<div class="product-img">
+												<img src="./img/<?php echo $value['image'] ?>" alt="">
+												<div class="product-label">
+													
+												</div>
+											</div>
+											<div class="product-body">
+												<p class="product-category">Category</p>
+												<h3 class="product-name"><a href="#"><?php echo $value['name'] ?></a></h3>
+												<h4 class="product-price"><?php echo number_format($value['price']) ?> VND</h4>
+												<div class="product-rating">
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+												</div>
+												<div class="product-btns">
+													<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
+													<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+												</div>
+											</div>
+											<div class="add-to-cart">
+												<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+											</div>
+										</div>
+										<!-- /product -->
+										<?php endforeach; ?>
+									</div>
+									<div id="slick-nav-10" class="products-slick-nav"></div>
+								</div>
+								<!-- /tab -->
+							</div>
+						</div>
+					</div>
+					<!-- /Products tab & slick -->
 				</div>
 				<!-- /row -->
 			</div>
 			<!-- /container -->
 		</div>
 		<!-- /SECTION -->
+
 		<!-- SECTION -->
 		<div class="section">
 			<!-- container -->
 			<div class="container">
 				<!-- row -->
 				<div class="row">
-					
+					<div class="col-md-4 col-xs-6">
+						<div class="section-title">
+							<h4 class="title">Top selling</h4>
+							<div class="section-nav">
+								<div id="slick-nav-11" class="products-slick-nav"></div>
+							</div>
+						</div>
+
+						<div class="products-widget-slick" data-nav="#slick-nav-11">
+							<?php foreach ($gettopsellingSmartphone as $sm):
+							foreach ($gettopsellingLaptops as $lt):
+							?>
+							<div>
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/<?php echo $sm['image']?>" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Category</p>
+										<h3 class="product-name"><a href="#"><?php echo $sm['name'] ?></a></h3>
+										<h4 class="product-price"><?php echo number_format($sm['price']) ?> VND</h4>
+									</div>
+								</div>
+								<!-- /product widget -->
+
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/<?php echo $lt['image'] ?>" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Category</p>
+										<h3 class="product-name"><a href="#"><?php echo $lt['name'] ?></a></h3>
+										<h4 class="product-price"><?php echo number_format($lt['price']) ?> VND</h4>
+									</div>
+								</div>
+								<!-- /product widget -->
+
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/product09.png" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Category</p>
+										<h3 class="product-name"><a href="#">product name goes here</a></h3>
+										<h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4>
+									</div>
+								</div>
+								<!-- product widget -->
+							</div>
+								<?php endforeach;
+								endforeach;
+								 ?>
+							
+						</div>
+					</div>
+
+					<div class="col-md-4 col-xs-6">
+						<div class="section-title">
+							<h4 class="title">Top selling</h4>
+							<div class="section-nav">
+								<div id="slick-nav-12" class="products-slick-nav"></div>
+							</div>
+						</div>
+
+						<div class="products-widget-slick" data-nav="#slick-nav-12">
+						<?php foreach ($gettopsellingSmartphone as $value):?>
+							<div>
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/<?php echo $value['image'] ?>" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Category</p>
+										<h3 class="product-name"><a href="#"><?php echo $value['name'] ?></a></h3>
+										<h4 class="product-price"><?php echo number_format($value['price']) ?>VND</h4>
+									</div>
+								</div>
+								<!-- product widget -->
+							</div>
+							<?php endforeach; ?>
+						</div>
+						<div class="products-widget-slick" >
+						<?php foreach ($gettopsellingLaptops as $value):?>
+							<div>
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/<?php echo $value['image'] ?>" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Category</p>
+										<h3 class="product-name"><a href="#"><?php echo $value['name'] ?></a></h3>
+										<h4 class="product-price"><?php echo number_format($value['price']) ?>VND</h4>
+									</div>
+								</div>
+								<!-- product widget -->
+							</div>
+							<?php endforeach; ?>
+						</div>
+						<div class="products-widget-slick">
+						<?php foreach ($gettopsellingAccessories as $value):?>
+							<div>
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/<?php echo $value['image'] ?>" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Category</p>
+										<h3 class="product-name"><a href="#"><?php echo $value['name'] ?></a></h3>
+										<h4 class="product-price"><?php echo number_format($value['price']) ?>VND</h4>
+									</div>
+								</div>
+								<!-- product widget -->
+							</div>
+							<?php endforeach; ?>
+						</div>
+					</div>
+
+					<div class="clearfix visible-sm visible-xs"></div>
+
+					<div class="col-md-4 col-xs-6">
+						<div class="section-title">
+							<h4 class="title">Top selling</h4>
+							<div class="section-nav">
+								<div id="slick-nav-13" class="products-slick-nav"></div>
+							</div>
+						</div>
+
+						<div class="products-widget-slick" data-nav="#slick-nav-13">
+							<?php foreach($gettopsellingSmartphone as $sm):
+									foreach ($gettopsellingLaptops as $lt):
+									foreach ($gettopsellingAccessories as $acs):
+								?>
+							<div>
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/<?php echo $sm['image'] ?>" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Category</p>
+										<h3 class="product-name"><a href="#"><?php $sm['name'] ?></a></h3>
+										<h4 class="product-price"><?php echo number_format($sm['price']) ?> VND</h4>
+									</div>
+								</div>
+								<!-- /product widget -->
+
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/<?php echo $lt['image'] ?>" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Category</p>
+										<h3 class="product-name"><a href="#"><?php echo $lt['name'] ?></a></h3>
+										<h4 class="product-price"><?php echo number_format($lt['price']) ?> VND</h4>
+									</div>
+								</div>
+								<!-- /product widget -->
+
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/<?php echo $acs['image'] ?>" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Category</p>
+										<h3 class="product-name"><a href="#"><?php echo $acs['name'] ?></a></h3>
+										<h4 class="product-price"><?php echo number_format($acs['price']) ?> VND</h4>
+									</div>
+								</div>
+								<!-- product widget -->
+							</div>
+								<?php endforeach;
+								endforeach;
+							endforeach;
+								?>
+						</div>
+					</div>
+
 				</div>
 				<!-- /row -->
 			</div>
--- models/product.php
+++ models/product.php
@@ -47,8 +47,8 @@ class Product extends Db
         $items = array();
         $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
         return $items; //return an array
-    }    
-    
+    }
+
     //get laptop    
     public function getLapTops()
     {
@@ -56,52 +56,57 @@ class Product extends Db
         $sql->execute();
         $items = array();
         $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
-        return $items;    }    
-        
+        return $items;
+    }
+
     //get smartphone   
-         public function getSmartPhone()
+    public function getSmartPhone()
     {
         $sql = self::$connection->prepare("SELECT * FROM `products`,`protypes` WHERE `products`.`type_id` = `protypes`.`type_id`AND `protypes`.`type_id` = 1");
         $sql->execute();
         $items = array();
         $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
-        return $items;    }    
-        
-        //get pc    
-        public function getPC()
+        return $items;
+    }
+
+    //get pc    
+    public function getPC()
     {
         $sql = self::$connection->prepare("SELECT * FROM `products`,`protypes` WHERE `products`.`type_id` = `protypes`.`type_id`AND `protypes`.`type_id` = 4");
         $sql->execute();
         $items = array();
         $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
-        return $items;    }    
-        
-        //get Accessories    
-        public function getAccessories()
+        return $items;
+    }
+
+    //get Accessories    
+    public function getAccessories()
     {
         $sql = self::$connection->prepare("SELECT * FROM `products`,`protypes` WHERE `products`.`type_id` = `protypes`.`type_id`AND `protypes`.`type_id` = 3");
         $sql->execute();
         $items = array();
         $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
-        return $items;    }    
-        
-        //get Tablet    
-        public function getTablet()
+        return $items;
+    }
+
+    //get Tablet    
+    public function getTablet()
     {
         $sql = self::$connection->prepare("SELECT * FROM `products`,`protypes` WHERE `products`.`type_id` = `protypes`.`type_id`AND `protypes`.`type_id` = 5");
         $sql->execute();
         $items = array();
         $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
-        return $items;    }   
-        
+        return $items;
+    }
+
 
     //get related products by type_id
     public function getRelatedProducts($type_id)
     {
         $sql = self::$connection->prepare("SELECT * FROM `products`, `protypes` WHERE `products`.`type_id` = `protypes`.`type_id` and `protypes`.`type_id` = ?");
-        $sql->bind_param("i",$type_id);
+        $sql->bind_param("i", $type_id);
         $sql->execute(); //return an object
- 
+
         $items = array();
         $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
         return $items; //return an array
@@ -127,11 +132,63 @@ class Product extends Db
         return $items; //return an array
     }
 
-    public function paginate($url, $total, $perPage)    {        $totalLinks = ceil($total / $perPage);
+    public function paginate($url, $total, $perPage)
+    {
+        $totalLinks = ceil($total / $perPage);
         $link = "";
         for ($j = 1; $j <= $totalLinks; $j++) {
             $link = $link . "<li><a href='$url&page=$j'> $j </a></li>";
         }
-        return $link;    }
+        return $link;
+    }
+    //Topselling Smartphone
+    public function gettopsellingSmartphone()
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products WHERE `type_id` = 1 AND `feature` = 1");
+        $sql->execute(); //return an object
 
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    //Topselling Laptops
+    public function gettopsellingLaptops()
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products WHERE `type_id` = 2 AND `feature` = 1");
+        $sql->execute(); //return an object
+
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    //Topselling Accessories
+    public function gettopsellingAccessoriess()
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products WHERE `type_id` = 3 AND `feature` = 1");
+        $sql->execute(); //return an object
+
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    //Topselling PC
+    public function gettopsellingPC()
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products WHERE `type_id` = 4 AND `feature` = 1");
+        $sql->execute(); //return an object
+
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    //Topselling Table
+    public function gettopsellingTablets()
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products WHERE `type_id` = 5 AND `feature` = 1");
+        $sql->execute(); //return an object
+
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
 }
